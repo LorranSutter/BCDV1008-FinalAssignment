8cc352061565dee4f7a8e35c7a38bd3e9b38d32f
Lorran Sutter <sutterlorran@gmail.com>
2020-04-07 00:35:42 -0400

    Sort messages by date in chat rooms

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -5,9 +5,10 @@ const History = require('../models/history');
 
 exports.index = async (req, res, next) => {
     const room = await Room.findById(req.params.roomId);
-    const msgList = await History.find({
-        room: mongoose.Types.ObjectId(req.params.roomId),
-    });
+    const msgList = await History.find(
+        { room: mongoose.Types.ObjectId(req.params.roomId) },
+        { _id: -1, user: 1, room: -1, message: 1, date: 1 }
+    ).sort({ 'date': 1 });
 
     res.cookie('roomId', req.params.roomId);
 
